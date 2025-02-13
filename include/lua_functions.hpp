db8a0c8be1a188fd88d64ba1190980dfde7aa6b8
zKiwiko <slimebomb33@gmail.com>
2025-02-13 15:15:47 -0500

    ...

--- include/lua_functions.hpp
+++ include/lua_functions.hpp
@@ -2,7 +2,12 @@
 
 #include <lua_state.h>
 
+namespace Astral 
+{
+
 class Lua_Functions {
 public:
     static void Register();
 };
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <windows.h>
 
+#include <lua_functions.h>
+
 int main(int argc, char* argv[]) {
     SetDllDirectoryA("bin");
+
     return 0;
 }
